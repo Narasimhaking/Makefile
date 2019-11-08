be06d7efff22f6381e075f6f8968ad12013e0e76
Narasimhaking <31504788+Narasimhaking@users.noreply.github.com>
2019-11-08 12:42:42 +0530

    sample c file for make
    testing make file with sample c file

---
+++ sample.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	#ifdef TEST
+	printf("TEST hello world\n");
+	#endif	
+	
+	#ifdef TEST2
+	printf("TEST2 hello world\n");	
+	#endif
+	
+	#ifdef TEST3
+	printf("TEST3 hello world\n");	
+	#endif
+}
